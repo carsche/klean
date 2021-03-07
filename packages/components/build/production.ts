fa410d6988dbe8fea46f755bf579cb6eda20f37c
Carl Johan Schedvin <carl.johan.schedvin@softhouse.se>
2021-03-07 11:15:45 +0000

    Improvements

--- packages/components/build/production.ts
+++ packages/components/build/production.ts
@@ -22,19 +22,8 @@ const configuration: webpack.Configuration = {
       }],
   },
   externals: {
-    // Don't bundle react or react-dom      
-    react: {
-      commonjs: "react",
-      commonjs2: "react",
-      amd: "React",
-      root: "React"
-    },
-    "react-dom": {
-      commonjs: "react-dom",
-      commonjs2: "react-dom",
-      amd: "ReactDOM",
-      root: "ReactDOM"
-    }
+    // Don't bundle react      
+    react: "react"
   }
 };
 
